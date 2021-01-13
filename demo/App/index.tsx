1aed85f2efc308afce647d38acb84b9ca7b7e5a6
Welly Shen <hivoid19@gmail.com>
2021-01-13 15:33:40 +0800

    Chore(demo): change back to demo app

--- demo/App/index.tsx
+++ demo/App/index.tsx
@@ -1,45 +1,72 @@
-import { FC, useState, useRef, useEffect } from "react";
+import { FC, useState } from "react";
 import { Global, css } from "@emotion/react";
 import normalize from "normalize.css";
 
+import GitHubCorner from "../GitHubCorner";
 import useDimensions from "../../src";
-import { root, container } from "./styles";
-
-const WrappedComp = () => {
-  const { ref, width, height } = useDimensions<HTMLDivElement>();
-
-  return (
-    <div
-      style={{ width: "300px", height: "300px", background: "red" }}
-      ref={ref}
-    >
-      {width} x {height}
-    </div>
-  );
-};
+import {
+  root,
+  container,
+  title,
+  subtitle,
+  frame,
+  bp,
+  controller,
+} from "./styles";
 
 const App: FC = () => {
-  // const ref = useRef(null);
-  const [show, setShow] = useState(true);
-  /* const {
+  const [size, setSize] = useState<{ w: number; h: number }>({
+    w: 250,
+    h: 300,
+  });
+  const {
     ref,
+    currentBreakpoint,
     width,
     height,
-    observe,
-    unobserve,
-  } = useDimensions<HTMLDivElement>({ ref }); */
+  } = useDimensions<HTMLDivElement>({
+    breakpoints: { XS: 0, SM: 100, MD: 200, LG: 300, XL: 400 },
+  });
 
-  /* useEffect(() => {
-    unobserve();
-    observe();
-  }, [observe, unobserve]);
+  const resize = (x: number, y: number) => {
+    if (!ref.current) return;
+    const { left: offsetX, top: offsetY } = ref.current.getBoundingClientRect();
+    setSize({ w: x - offsetX, h: y - offsetY });
+  };
 
-  const setRef = (el: any) => {
-    if (el) observe(el);
-  }; */
+  const handleMouseMove = (e: MouseEvent) => {
+    e.preventDefault();
+    resize(e.pageX, e.pageY);
+  };
+
+  const handleTouchMove = (e: TouchEvent) => {
+    resize(e.touches[0].pageX, e.touches[0].pageY);
+  };
+
+  const handleDragStart = () => {
+    if (typeof window.ontouchstart === "undefined") {
+      document.addEventListener("mousemove", handleMouseMove);
+      document.addEventListener(
+        "mouseup",
+        () => {
+          document.removeEventListener("mousemove", handleMouseMove);
+        },
+        { once: true }
+      );
+    } else {
+      document.addEventListener("touchmove", handleTouchMove);
+      document.addEventListener(
+        "touchend",
+        () => {
+          document.removeEventListener("touchmove", handleTouchMove);
+        },
+        { once: true }
+      );
+    }
+  };
 
   return (
-    <div>
+    <>
       <Global
         styles={css`
           ${normalize}
@@ -47,12 +74,30 @@ const App: FC = () => {
         `}
       />
       <div css={container}>
-        <button type="button" onClick={() => setShow(!show)}>
-          Toggle
-        </button>
-        {show && <WrappedComp />}
+        <GitHubCorner url="https://github.com/wellyshen/react-cool-dimensions" />
+        <h1 css={title}>REACT COOL DIMENSIONS</h1>
+        <p css={subtitle}>
+          React hook to measure an element&apos;s size and handle responsive
+          components.
+        </p>
+        <div
+          css={frame}
+          style={{ width: `${size.w}px`, height: `${size.h}px` }}
+          ref={ref}
+        >
+          <div css={bp}>{currentBreakpoint}</div>
+          <div>
+            {Math.floor(width)} x {Math.floor(height)}
+          </div>
+          {/* eslint-disable-next-line jsx-a11y/no-static-element-interactions */}
+          <div
+            css={controller}
+            onMouseDown={handleDragStart}
+            onTouchStart={handleDragStart}
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
